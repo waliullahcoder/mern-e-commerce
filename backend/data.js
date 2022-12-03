89ad56f2ad873607d2277f357b1bfc35c8743650
waliullahcoder <waliullahbiplob786@gmail.com>
2022-12-03 17:01:07 +0600

    Part11: Complete-Add-To-Cart

--- backend/data.js
+++ backend/data.js
@@ -1,6 +1,7 @@
 const data = {
     products: [
-      {
+      {      
+        _id: '1',
         name: 'Nike Slim shirt',
         slug: 'nike-slim-shirt',
         category: 'Shirts',
@@ -12,7 +13,8 @@ const data = {
         numReviews: 10,
         description: 'high quality shirt',
       },
-      {
+      {      
+        _id: '2',
         name: 'Adidas Fit Shirt',
         slug: 'adidas-fit-shirt',
         category: 'Shirts',
@@ -24,7 +26,8 @@ const data = {
         numReviews: 10,
         description: 'high quality product',
       },
-      {
+      {      
+        _id: '3',
         name: 'Nike Slim Pant',
         slug: 'nike-slim-pant',
         category: 'Pants',
@@ -37,6 +40,7 @@ const data = {
         description: 'high quality product',
       },
       {
+        _id: '4',
         name: 'Adidas Fit Pant',
         slug: 'adidas-fit-pant',
         category: 'Pants',
